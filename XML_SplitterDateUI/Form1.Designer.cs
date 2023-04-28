21194952a0900bd75e2a0cd49095740f624b4c29
Paul Thomson <Pthomson@wgeld.org>
2023-04-28 07:53:33 -0400

    Add project files.

---
+++ XML_SplitterDateUI/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace XML_SplitterDateUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BeginningDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.EndingDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectSourceFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectSaveLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BeginningDateTimePicker
+            // 
+            this.BeginningDateTimePicker.Location = new System.Drawing.Point(60, 104);
+            this.BeginningDateTimePicker.Name = "BeginningDateTimePicker";
+            this.BeginningDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.BeginningDateTimePicker.TabIndex = 0;
+            // 
+            // EndingDateTimePicker
+            // 
+            this.EndingDateTimePicker.Location = new System.Drawing.Point(415, 104);
+            this.EndingDateTimePicker.Name = "EndingDateTimePicker";
+            this.EndingDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.EndingDateTimePicker.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Beginning Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ending Date";
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(246, 180);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(168, 79);
+            this.RunButton.TabIndex = 4;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(694, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(694, 24);
+            this.menuStrip2.TabIndex = 6;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectSourceFolderToolStripMenuItem,
+            this.selectSaveLocationToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // selectSourceFolderToolStripMenuItem
+            // 
+            this.selectSourceFolderToolStripMenuItem.Name = "selectSourceFolderToolStripMenuItem";
+            this.selectSourceFolderToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.selectSourceFolderToolStripMenuItem.Text = "Select Source Folder";
+            this.selectSourceFolderToolStripMenuItem.Click += new System.EventHandler(this.selectSourceFolderToolStripMenuItem_Click);
+            // 
+            // selectSaveLocationToolStripMenuItem
+            // 
+            this.selectSaveLocationToolStripMenuItem.Name = "selectSaveLocationToolStripMenuItem";
+            this.selectSaveLocationToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.selectSaveLocationToolStripMenuItem.Text = "Select Save Location";
+            this.selectSaveLocationToolStripMenuItem.Click += new System.EventHandler(this.selectSaveLocationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 335);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EndingDateTimePicker);
+            this.Controls.Add(this.BeginningDateTimePicker);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker BeginningDateTimePicker;
+        private System.Windows.Forms.DateTimePicker EndingDateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectSourceFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectSaveLocationToolStripMenuItem;
+    }
+}
+
---
+++ XML_SplitterDateUI/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using System.Threading;
+using System.Web;
+using XML_SplitterDateUI.Properties;
+
+namespace XML_SplitterDateUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        private List<DateTime> DateList = new List<DateTime>();
+        public List<DateTime> GetList()
+        {
+            return DateList;
+        }
+
+            private void RunButton_Click(object sender, EventArgs e)
+            {
+            var beginningDate = BeginningDateTimePicker.Value;
+            var endingDate = EndingDateTimePicker.Value;
+
+            var confirmResult = MessageBox.Show($"Are these dates correct? ({beginningDate.ToString("MM/dd/yyyy")} to {endingDate.ToString("MM/dd/yyyy")})\n\nWarning: Process may take time.", "Confirm", MessageBoxButtons.YesNo);
+            var DaysDifference = Math.Abs(beginningDate.Day - endingDate.Day);
+            // creates list that stores each day between the start and end date 
+            for (int day = 0; day <= DaysDifference; day++)
+            {
+                DateList.Add(beginningDate);
+                beginningDate = beginningDate.AddDays(1);
+            }
+
+            if (confirmResult == DialogResult.Yes)
+            {
+                Split();
+                DateList.Clear(); // resets list to prevent duplicates if run button is pressed more than once
+            }
+        }
+       
+        public void Split()
+        {
+            string saveLocation = Settings.Default["saveLocation"].ToString();
+            string xmlLocation = Settings.Default["xmlLocation"].ToString();
+            DateTime today = DateTime.Today;
+            Stopwatch stopWatch = new Stopwatch();
+
+            int index = 0;
+            string previousFileDate = "";
+
+            //string saveLocation = "\\\\Automate2101\\d\\Applications\\MDMIntervalDataParser\\DailyMDMDataOutput";
+            //string xmlLocation = "\\\\Automate2101\\d\\Applications\\AMRDailyReadProcessing\\FullIntDataSource";
+
+
+            string filePath = "";
+
+            stopWatch.Start();
+
+            foreach (var path in Directory.GetFiles(@xmlLocation))
+            {
+                String fileDate = GetFileDate(System.IO.Path.GetFileName(path).Substring(20));
+
+                
+                foreach (var day in this.GetList())
+                {
+                   
+                    if (fileDate.Equals(day.ToString("MM/dd/yyyy")))    // if the file's date matches the date in list proceed
+                    {
+                        previousFileDate = day.ToString("MM/dd/yyyy");
+                        filePath = xmlLocation + "\\" + System.IO.Path.GetFileName(path);    // instantiate the filePath to equal the xmlLocation and the file being examined
+
+                        XDocument newDoc = XDocument.Load(filePath);
+
+                        XmlReader xmlReader = newDoc.CreateReader();
+
+
+                        // For AMRDEF Element
+                        string amrdefPurpose = "";
+                        string amrdefVersion = "";
+                        string amrdefCreationTime = "";
+
+                        // For ScheduleExecution Element
+                        string scheduleExecutionIrn = "";
+                        string scheduleExecutionStarted = "";
+                        string scheduleExecutionFinished = "";
+                        string scheduleExecutionInitiator = "";
+
+
+                        /* gathers the values for each attribute and stores it for use in the final output */
+                        while (xmlReader.Read())
+                        {
+                            if (xmlReader.Name.Equals("AMRDEF")) // If "AMRDEF" Element is found instantiate the following attribute values.
+                            {
+                                amrdefPurpose = xmlReader.GetAttribute("Purpose");
+                                amrdefVersion = xmlReader.GetAttribute("version");
+                                amrdefCreationTime = xmlReader.GetAttribute("CreationTime");
+                            }
+
+                            if (xmlReader.Name.Equals("ScheduleExecution")) // If "ScheduleExecution" Element is found instantiate the following attribute values.
+                            {
+                                scheduleExecutionIrn = xmlReader.GetAttribute("Irn");
+                                scheduleExecutionStarted = xmlReader.GetAttribute("started");
+                                scheduleExecutionFinished = xmlReader.GetAttribute("finished");
+                                scheduleExecutionInitiator = xmlReader.GetAttribute("Initiator");
+                                break;
+                            }
+                        }
+
+
+
+                        var query = newDoc.Root.Descendants("ScheduleExecution").DescendantNodes(); // Get all nodes found within, but not including, the "SceduleExecution" Element
+                        XElement header = new XElement("ScheduleExecution", new XAttribute("Irn", scheduleExecutionIrn),
+                            new XAttribute("started", scheduleExecutionStarted),
+                            new XAttribute("finished", scheduleExecutionFinished),
+                            new XAttribute("Initiator", scheduleExecutionInitiator), query);  // Take the Header ("ScheduleExecution") of the current XML File being examined
+
+                        newDoc.Descendants("ScheduleExecution").Remove(); // Remove the Header ("ScheduleExecution") from the newDoc object
+
+                        int elementsPerFile = GetNumberOfElementsPerNewFile(newDoc);
+
+                        // adds {elementsPerFile} elements to a new file that then saves to a folder
+                        foreach (var batch in newDoc.Root.Elements().InSetsOf(elementsPerFile))
+                        {
+                            var finalDoc = new XDocument(
+                                 new XElement("AMRDEF", new XAttribute("Purpose", amrdefPurpose), new XAttribute("version", amrdefVersion), new XAttribute("CreationTime", amrdefCreationTime), header, batch));
+                            finalDoc.Save($"{saveLocation}\\meterDataFile_{day.ToString("MM/dd/yyyy").Replace("/", "_")}_{++index}.xml");
+                            System.GC.Collect(); // attempt to reduce unnecessary memory usage
+                        }
+                    }
+                    else if (fileDate.Equals(previousFileDate) == false)
+                    {
+                        index = 0;
+                    }
+
+                }
+            }
+
+            stopWatch.Stop();
+            MessageBox.Show($"Process Completed.\n\nTime Elapsed: {stopWatch.Elapsed.ToString("mm\\:ss")}");
+
+            /****************************************************************************/
+            /*The Following modifies the file's date in the format MM/dd/yyy*/
+            static string GetFileDate(string fileDate)
+            {
+                String date = fileDate;
+                date = date.Replace('_', '/');
+                date = date.Substring(0, 10);
+                return date;
+            }
+
+            /***************************************************************************/
+            /* Below Counts the Original XML file's Elements which then determines, and returns, how many
+             * Elements each new file showed display */
+            static int GetNumberOfElementsPerNewFile(XDocument newDoc)
+            {
+                double scalarVariableCount = newDoc.Root.Elements().Count();
+                double numberOfElementsPerFile = Math.Ceiling(scalarVariableCount / 24);
+                int elementAmount = Convert.ToInt32(numberOfElementsPerFile);
+
+                return elementAmount;
+            }
+
+        }
+
+
+        // Selects and stores source files folder location 
+        private void selectSourceFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.SelectedPath = Properties.Settings.Default["LastSelectedXMLFolder"].ToString();
+            folder.Description = ("Please select the folder in which the source files are stored.");
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                Settings.Default["xmlLocation"] = folder.SelectedPath;
+                Properties.Settings.Default["LastSelectedXMLFolder"] = folder.SelectedPath.ToString();
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        // Selects and stores saved files folder location 
+        private void selectSaveLocationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.SelectedPath = Properties.Settings.Default["LastSelectedSaveFolder"].ToString();
+            folder.Description = ("Please select a folder to save the files to.");
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                Settings.Default["saveLocation"] = folder.SelectedPath;
+                Properties.Settings.Default["LastSelectedSaveFolder"] = folder.SelectedPath.ToString();
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+
+    /*********************************************************************************************/
+    /**** This class assists in determing how many elements should be included in one "batch" ****/
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<List<T>> InSetsOf<T>(this IEnumerable<T> source, int max)
+        {
+            List<T> toReturn = new List<T>(max);
+            foreach (var item in source)
+            {
+                toReturn.Add(item);
+                if (toReturn.Count == max)
+                {
+                    yield return toReturn;
+                    toReturn = new List<T>(max);
+                }
+            }
+            if (toReturn.Any())
+            {
+                yield return toReturn;
+            }
+        }
+    }
+}
+
